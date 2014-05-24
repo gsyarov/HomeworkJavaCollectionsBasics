1e8e29ce75468e29931b34e63f84bfd6c2e6f022
Gabriel Syarov <gsyarov@gmail.com>
2014-05-24 14:07:46 +0300

    Problem 3

---
+++ src/_03_LargestSequenceOfEqualStrings.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class _03_LargestSequenceOfEqualStrings {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String elementString = scanner.nextLine();
+		String[] elementArray = elementString.split(" ");
+		int counter = 1;
+		int maxCount = 1;
+		int position = 0;
+		
+		for (int i = 1; i < elementArray.length; i++) {
+			if (elementArray[i].equals(elementArray[i - 1])) {
+				counter++;
+			} else {
+				counter = 1;
+			}
+			if (maxCount < counter) {
+				maxCount = counter;
+				position = i;
+			}
+		}
+		
+		for (int j = 0; j < counter - 1; j++) {
+			System.out.print(elementArray[position] + " ");
+		}
+		System.out.println(elementArray[position]);
+	}
+}

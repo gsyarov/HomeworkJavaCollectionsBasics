93bce04e3b6557f1c6d2176c6832a3786a6ea41e
Gabriel Syarov <gsyarov@gmail.com>
2014-05-24 13:27:51 +0300

    Problem 1 finish

--- src/_01_SortArrayOfNumbers.java
+++ src/_01_SortArrayOfNumbers.java
@@ -1,14 +1,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 public class _01_SortArrayOfNumbers {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		int[] numbers =new int[n];
+		int[] numbers = new int[n];
 		for (int i = 0; i < n; i++) {
 			numbers[i] = scanner.nextInt();
 		}

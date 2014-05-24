5b412deb45d3089667fef9a29d778ebe6eb11a68
Gabriel Syarov <gsyarov@gmail.com>
2014-05-24 13:26:50 +0300

    Finish first problem

--- src/_01_SortArrayOfNumbers.java
+++ src/_01_SortArrayOfNumbers.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 public class _01_SortArrayOfNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] numbers =new int[n];
+		for (int i = 0; i < n; i++) {
+			numbers[i] = scanner.nextInt();
+		}
+		Arrays.sort(numbers);
+		System.out.println(Arrays.toString(numbers));
 	}
 
 }
